015587ec1710a87c0b1a2ff1000f6b330a144e05
Dan Schatzberg <schatzberg.dan@gmail.com>
2015-08-05 07:57:59 -0500

    Add Kvm::open
    Signed-off-by: Dan Schatzberg <schatzberg.dan@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::fs;
+
+pub struct Kvm {
+    handle: fs::File
+}
+
+impl Kvm {
+    pub fn open() -> io::Result<Kvm> {
+        let f = try!(fs::File::open("/dev/kvm"));
+        Ok(Kvm { handle: f })
+    }
+}
+
 #[test]
-fn it_works() {
+fn open_test() {
+    Kvm::open().unwrap();
 }
