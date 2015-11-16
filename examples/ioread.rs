b395fef964b593c0fa151f2475cae4c8751e56f1
Dan Schatzberg <schatzberg.dan@gmail.com>
2015-11-16 13:49:17 -0500

    Update and document example
    Signed-off-by: Dan Schatzberg <schatzberg.dan@gmail.com>

--- examples/ioread.rs
+++ examples/ioread.rs
@@ -1,30 +1,44 @@
+// In this example we will construct a single CPU x86 VM which will execute
+// "inb 0x01" at ring 0 with paging disabled
+
 extern crate kvm;
 extern crate memmap;
 
-use kvm::{Exit, IoDirection, System, Vcpu, VirtualMachine};
+use kvm::{Capability, Exit, IoDirection, System, Vcpu, VirtualMachine};
 use memmap::{Mmap, Protection};
 
 fn main() {
+    // Allocate 2MB for the guest memory
     let mut anon_mmap = Mmap::anonymous(2 * (1 << 20), Protection::ReadWrite)
                             .unwrap();
     let slice = unsafe { anon_mmap.as_mut_slice() };
 
+    // Write the "inb 0x01" instruction at 1MB
     slice[0x100000] = 0xe4;
     slice[0x100001] = 0x01;
 
+    // Initialize the KVM system
     let sys = System::initialize().unwrap();
 
+    // Create a Virtual Machine
     let mut vm = VirtualMachine::create(&sys).unwrap();
 
+    // Ensure that the VM supports memory backing with user memory
+    assert!(vm.check_capability(Capability::UserMemory) > 0);
+    // Set the 2 MB range to start at physical address 0
     vm.set_user_memory_region(0, slice, 0).unwrap();
 
+    // Create a new VCPU
     let mut vcpu = Vcpu::create(&mut vm).unwrap();
 
+    // Set supported CPUID (KVM fails without doing this)
     let mut cpuid = sys.get_supported_cpuid().unwrap();
     vcpu.set_cpuid2(&mut cpuid).unwrap();
 
+    // Setup the special registers
     let mut sregs = vcpu.get_sregs().unwrap();
 
+    // Set the code segment to have base 0, limit 4GB (flat segmentation)
     sregs.cs.base = 0x0;
     sregs.cs.limit = 0xffffffff;
     sregs.cs.selector = 0x8;
@@ -37,76 +51,24 @@ fn main() {
     sregs.cs.g = 1;
     sregs.cs.avl = 0;
 
-    sregs.ss.base = 0x0;
-    sregs.ss.limit = 0xffffffff;
-    sregs.ss.selector = 0;
-    sregs.ss._type = 0;
-    sregs.ss.present = 0;
-    sregs.ss.dpl = 0;
-    sregs.ss.db = 1;
-    sregs.ss.s = 0;
-    sregs.ss.l = 0;
-    sregs.ss.g = 1;
-    sregs.ss.avl = 0;
-
-    sregs.ds.base = 0x0;
-    sregs.ds.limit = 0xffffffff;
-    sregs.ds.selector = 0;
-    sregs.ds._type = 0;
-    sregs.ds.present = 0;
-    sregs.ds.dpl = 0;
-    sregs.ds.db = 1;
-    sregs.ds.s = 0;
-    sregs.ds.l = 0;
-    sregs.ds.g = 1;
-    sregs.ds.avl = 0;
-
-    sregs.es.base = 0x0;
-    sregs.es.limit = 0xffffffff;
-    sregs.es.selector = 0;
-    sregs.es._type = 0;
-    sregs.es.present = 0;
-    sregs.es.dpl = 0;
-    sregs.es.db = 1;
-    sregs.es.s = 0;
-    sregs.es.l = 0;
-    sregs.es.g = 1;
-    sregs.es.avl = 0;
-
-    sregs.fs.base = 0x0;
-    sregs.fs.limit = 0xffffffff;
-    sregs.fs.selector = 0;
-    sregs.fs._type = 0;
-    sregs.fs.present = 0;
-    sregs.fs.dpl = 0;
-    sregs.fs.db = 1;
-    sregs.fs.s = 0;
-    sregs.fs.l = 0;
-    sregs.fs.g = 1;
-    sregs.fs.avl = 0;
-
-    sregs.gs.base = 0x0;
-    sregs.gs.limit = 0xffffffff;
-    sregs.gs.selector = 0;
-    sregs.gs._type = 0;
-    sregs.gs.present = 0;
-    sregs.gs.dpl = 0;
-    sregs.gs.db = 1;
-    sregs.gs.s = 0;
-    sregs.gs.l = 0;
-    sregs.gs.g = 1;
-    sregs.gs.avl = 0;
-
+    // We don't need to populate the GDT if we have our segments setup
+    // cr0 - protected mode on, paging disabled
     sregs.cr0 = 0x50033;
-    sregs.cr4 = 0x1046b0;
 
+    // Set the special registers
     vcpu.set_sregs(&sregs).unwrap();
 
     let mut regs = vcpu.get_regs().unwrap();
+    // set the instruction pointer to 1 MB
     regs.rip = 0x100000;
-    regs.rflags = 0x246;
+    regs.rflags = 0x2;
     vcpu.set_regs(&regs).unwrap();
+
+    // Actually run the VCPU
     let run = unsafe { vcpu.run() }.unwrap();
+
+    // Ensure that the exit reason we get back indicates that the I/O
+    // instruction was executed
     assert!(run.exit_reason == Exit::Io);
     let io = unsafe { *run.io() };
     assert!(io.direction == IoDirection::In);
